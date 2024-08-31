7cb4663ba1a03f91f2b5e10d6fafa08db63d39a4
Mara <mlevy2525@gmail.com>
2024-08-31 16:17:09 -0400

    quick fix

--- src/models/dift_sd.py
+++ src/models/dift_sd.py
@@ -189,7 +189,7 @@ class OneStepSDPipeline(StableDiffusionPipeline):
 
 class SDFeaturizer:
     def __init__(self, sd_id='stabilityai/stable-diffusion-2-1', null_prompt='', device='cuda'):
-        self.device
+        self.device = device
         unet = MyUNet2DConditionModel.from_pretrained(sd_id, subfolder="unet")
         onestep_pipe = OneStepSDPipeline.from_pretrained(sd_id, unet=unet, safety_checker=None)
         onestep_pipe.vae.decoder = None
